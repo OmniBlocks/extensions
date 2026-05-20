269bf0bebfad88af0d4f59372d13ffb98aebc4ae
GraisonAtSchoolAgain <gparkhurst954@lsr7.net>
2026-05-20 10:36:09 -0500

    Create brainfckinterpreter.js placeholder (for now)

---
+++ extensions/GvYoutube/brainfckinterpreter.js
@@ -0,0 +1,7 @@
+// Name: Brainf**k Interpreter
+// ID: gvyoutubebrainfck
+// Description: Interpreter for the Brainf**k language.
+// By: GvYoutube <https://scratch.mit.edu/users/GvYoutube/>
+// License: MIT
+// Tags: code interperter
+
